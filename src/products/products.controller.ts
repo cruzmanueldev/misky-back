5355809abb20fede5fccd878cc0ae6bda987c140
cmanuelcabbage <cmanuel.cabbage@gmail.com>
2025-10-11 11:46:48 -0500

    1110 Apis Products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Get, Query, Patch,
   Body,
   UploadedFile,
   UseInterceptors,
+  Post,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
@@ -17,16 +18,82 @@ export class ProductsController {
   findAll() {
     return this.productsService.findAll();
   }
-
+  
   @Get('top')
   findTopProducts() {
     return this.productsService.findTopProducts();
+  }
+
+  @Get('promotion')
+  findPromotionProduct() {
+    return this.productsService.findPromotionProduct();
+  }
+
+  @Patch('top')
+  updateTopProducts(@Body('ids') ids: number[]) {
+    return this.productsService.updateTopProducts(ids);
+  }
+
+  @Patch('promotion/:id')
+  updateProductPromotion(@Param('id') id: string) {
+    return this.productsService.updateProductPromotion(Number(id));
+  }
+
+  @Post('detail/:id')
+  updateDetailProduct(
+    @Param('id') id: string,
+    @Body('details') details: String[]
+  ) {
+    return this.productsService.updateDetailProduct(Number(id), details);
+  }
+
+  @Get('top-selling')
+  findTopSellingProducts() {
+    return this.productsService.findTopSellingProducts();
+  }
+
+  @Patch('top-selling')
+  updateTopSellingProducts(@Body('ids') ids: number[]) {
+    return this.productsService.updateTopSellingProducts(ids);
   }
 
   @Get('/category')
   getProducts(@Query('category') category: number) {
     return this.productsService.findByCategory(category);
   }
+
+  @Get(':id')
+  findProduct(@Param('id') id: string) {
+    return this.productsService.findProduct(Number(id));
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads/products',
+        filename: (req, file, cb) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(null, uniqueSuffix + extname(file.originalname));
+        },
+      }),
+    }),
+  )
+  async createProduct(
+    @Body() body: any,
+    @UploadedFile() file?: Express.Multer.File,
+  ) {
+    const tags =
+      typeof body.tags === 'string' ? JSON.parse(body.tags) : body.tags;
+
+    return this.productsService.createProduct({
+      ...body,
+      image: file?.filename,
+      tags, // 👈 aquí pasas tu array
+    });
+  }
+
 
   @Patch(':id')
   @UseInterceptors(
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -18,6 +18,7 @@ export class ProductsService {
             name: true,
             price: true,
             description: true,
+            detail:true,
             order: true,
             image: true,
             units: true,
@@ -33,14 +34,167 @@ export class ProductsService {
     })
   }
 
+  findProduct(id: number) {
+    return this.prisma.product.findUnique({
+      where: {
+        id: id
+      },
+      select: {
+        id: true,
+          name: true,
+          price: true,
+          description: true,
+          detail:true,
+          order: true,
+          image: true,
+          units: true,
+          category_id: true,
+          detailpack : {
+            select : {
+              id: true,
+              name : true
+            }
+          }
+      },
+    });
+  }
+
   findTopProducts() {
     return this.prisma.product.findMany({
+      // where: {
+      //   show_in_carrousel: true
+      // },
+      where : {
+        id: {
+          in : [ 7 , 15, 35, 9, 41, 2,  21, 8]
+        }
+      },
       take: 8,
       select: {
         id: true,
           name: true,
           price: true,
           description: true,
+          detail:true,
+          order: true,
+          image: true,
+          units: true,
+          category_id: true,
+          detailpack : {
+            select : {
+              name : true
+            }
+          }
+      },
+    });
+  }
+
+  findPromotionProduct() {
+    return this.prisma.product.findFirst({
+      where: {
+        active_promotion: true
+      },
+      select: {
+        id: true,
+          name: true,
+          price: true,
+          description: true,
+          detail:true,
+          order: true,
+          image: true,
+          units: true,
+          category_id: true,
+          detailpack : {
+            select : {
+              name : true
+            }
+          }
+      },
+    });
+  }
+
+  async updateProductPromotion(id: number) {
+
+    await this.prisma.product.updateMany({
+      data: { show_in_carrousel: false },
+    });
+
+    return this.prisma.product.update({
+      where: { id },
+      data: {
+        active_promotion: true,
+      },
+    });
+
+  }
+
+  async updateDetailProduct(id: Number, details: String[]) {
+
+    await this.prisma.detailpack.deleteMany({
+      where : {
+        product_id : Number(id),
+      },
+    });
+
+    const detail_product = details.map((detail: string) => ({ 
+      name: detail, 
+      product_id: Number(id),
+      updated_at: new Date() 
+    }));
+    
+    const detail = await this.prisma.detailpack.createMany({
+      data : detail_product
+    })
+  }
+
+  async updateTopProducts(ids: number[]) {
+    await this.prisma.product.updateMany({
+      data: { show_in_carrousel: false },
+    });
+
+    if (ids && ids.length > 0) {
+      await this.prisma.product.updateMany({
+        where: { id: { in: ids } },
+        data: { show_in_carrousel: true },
+      });
+    }
+
+    return this.prisma.product.findMany({
+      where: { show_in_carrousel: true },
+    });
+  }
+
+  async updateTopSellingProducts(ids: number[]) {
+    await this.prisma.product.updateMany({
+      data: { best_seller: false },
+    });
+
+    if (ids && ids.length > 0) {
+      await this.prisma.product.updateMany({
+        where: { id: { in: ids } },
+        data: { best_seller: true },
+      });
+    }
+
+    return this.prisma.product.findMany({
+      where: { best_seller: true },
+    });
+  }
+
+  findTopSellingProducts() {
+    return this.prisma.product.findMany({
+      where : {
+        id: {
+          in : [ 7 , 15, 35, 9, 41, 2,  21, 8]
+        }
+      },
+      // take: 8,
+      select: {
+        id: true,
+          name: true,
+          price: true,
+          description: true,
+          detail:true,
           order: true,
           image: true,
           units: true,
@@ -69,6 +223,7 @@ export class ProductsService {
             name: true,
             price: true,
             description: true,
+            detail:true,
             order: true,
             image: true,
             units: true,
@@ -84,6 +239,38 @@ export class ProductsService {
     })
   }
 
+  async createProduct(data: any) {
+
+    const { 
+      name, 
+      price, 
+      image,
+      description,
+      detail,
+      description_carrousel, 
+      category_id,
+      units,
+    } = data;
+
+    const product = await this.prisma.product.create({
+      data: {
+        name,
+        price: Number(price),
+        image,
+        detail,
+        description,
+        units: Number(units),
+        description_carrousel,
+        category_id: Number(category_id),
+        updated_at: new Date(),
+      },
+    });
+    
+    return product;
+  }
+
+
+
   async updateProduct(id: number, data: any, imageName?: string) {
     const existing = await this.prisma.product.findUnique({ where: { id } });
     if (!existing) throw new NotFoundException('Producto no encontrado');
@@ -93,7 +280,10 @@ export class ProductsService {
       price: data.price ? Number(data.price) : existing.price,
       description: data.description ?? existing.description,
       order: data.order ?? existing.order,
+      detail: data.detail ?? existing.detail,
       units: data.units ?? existing.units,
+      category_id: data.category_id ? Number(data.category_id) : existing.category_id,
+      description_carrousel: data.description_carrousel ?? existing.description_carrousel,
     };
 
     if (imageName) {
